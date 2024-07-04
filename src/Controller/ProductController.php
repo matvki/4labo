9ac2976e67082d336e28c04649b2025c1ad50357
matvki <mat.vecchi007@gmail.com>
2024-07-04 19:15:25 +0000

    start crud for product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/product', name: 'product')]
+class ProductController extends AbstractController
+{
+    #[Route('', name: '')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
