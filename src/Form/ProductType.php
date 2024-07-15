265fd6dce950d610a5734a42a82759a225330d44
matvki <mat.vecchi007@gmail.com>
2024-07-15 16:51:11 +0000

    product crud

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,14 +9,16 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('price', TextType::class)
             ->add('user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
