b2de0829541270a97f351024079fbadd384841ca
matvki <mat.vecchi007@gmail.com>
2024-07-26 07:12:26 +0000

    update dependancies

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Product;
 
 class HomeController extends AbstractController
 {
