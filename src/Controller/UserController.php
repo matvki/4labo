eb63dd45290f0b22ab9f894b6d5c9ffec34df444
matvki <mat.vecchi007@gmail.com>
2024-07-27 15:06:38 +0000

    patch bootstrap error

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,16 +20,8 @@ class UserController extends AbstractController
         $this->entityManager    = $entityManager;
     }
 
-    #[Route('', name: '')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-
     // get a specific user by id
-    #[Route('/get/{user}', name: '_show')]
+    #[Route('/show/{user}', name: '_profile')]
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
@@ -37,60 +29,5 @@ class UserController extends AbstractController
         ]);
     }
 
-    // create a new user from a form
-    #[Route('/create', name: '_create')]
-    public function create(Request $request): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('user_show', ['user' => $user->getId()]);
-        }
-
-        return $this->render('user/createEdit.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    // edit an existing user
-    #[Route('/edit/{user}', name: '_edit')]
-    public function edit(Request $request, User $user): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('user_show', ['user' => $user->getId()]);
-        }
-
-        return $this->render('user/createEdit.html.twig', [
-            'form' => $form->createView(),
-            'user' => $user,
-        ]);
-    }
-
-    // delete an existing user
-    #[Route('/delete/{user}', name: '_delete')]
-    public function delete(User $user): Response
-    {
-        $this->entityManager->remove($user);
-        $this->entityManager->flush();
-
-        return $this->redirectToRoute('user');
-    }
-
-    #[Route('/success', name: '_success')]
-    public function success(): Response
-    {
-        return $this->render('user/success.html.twig');
-    }
 }
