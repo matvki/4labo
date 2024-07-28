348c619ce3e42c4dd0538da3b3685f98e482c21b
matvki <mat.vecchi007@gmail.com>
2024-07-28 12:39:05 +0000

    updated add product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,18 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Product;
+use App\Entity\Media;
 use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/product', name: 'product')]
 class ProductController extends AbstractController
 {
     private Request $request;
+    private SluggerInterface $slugger;
+    private ValidatorInterface $validator;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, SluggerInterface $slugger, ValidatorInterface $validator)
     {
         $this->entityManager    = $entityManager;
+        $this->slugger          = $slugger;
+        $this->validator        = $validator;
     }
 
     #[Route('', name: '')]
@@ -36,51 +43,50 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/create', name: '_create')]
-    public function create(Request $request): Response
+    #[Route('/new', name: '_new')]
+    public function new(Request $request): Response
     {
-        $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-
+        $product    = new Product();
+        $form       = $this->createForm(ProductType::class, $product);
+       
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($product);
-            $this->entityManager->flush();
+        if ($form->isSubmitted()) {
+            $mediaFiles = $form->get('media')->getData();
 
-            return $this->redirectToRoute('product_show', ['product' => $product->getId()]);
+            // Encode media files in base64
+            $mediaEntities = [];
+            foreach ($mediaFiles as $mediaFile) {
+                $base64Content = base64_encode(file_get_contents($mediaFile->getPathname()));
+
+                $media = new Media();
+                $media->setMedia($base64Content);
+                $media->setProduct($product);
+                $mediaEntities[] = $media;
+            }
+
+            $errors = $this->validator->validate($product);
+
+            if (count($errors) == 0) {
+                foreach ($mediaEntities as $media) {
+                    $this->entityManager->persist($media);
+                }
+
+                $product->setUser($this->getUser()); // Set the user
+                $this->entityManager->persist($product);
+                $this->entityManager->flush();
+
+                return $this->redirectToRoute('user_profile', ['user' => $this->getUser()->getId()]);
+            } else {
+                foreach ($errors as $error) {
+                    $this->addFlash('error', $error->getMessage());
+                }
+            }
         }
 
-        return $this->render('product/createEdit.html.twig', [
-            'form' => $form->createView(),
+        return $this->render('product/create.html.twig', [
+            'product'   => $product,
+            'form'      => $form->createView(),
         ]);
-    }
-
-    #[Route('/edit/{product}', name: '_edit')]
-    public function edit(Request $request, Product $product): Response
-    {
-        $form = $this->createForm(ProductType::class, $product);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('product_show', ['product' => $product->getId()]);
-        }
-
-        return $this->render('product/createEdit.html.twig', [
-            'form' => $form->createView(),
-            'product' => $product,
-        ]);
-    }
-
-    #[Route('/delete/{product}', name: '_delete')]
-    public function delete(Product $product): Response
-    {
-        $this->entityManager->remove($product);
-        $this->entityManager->flush();
-
-        return $this->redirectToRoute('product');
     }
 }
